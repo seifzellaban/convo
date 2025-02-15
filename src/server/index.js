f0ea686df1f2be555a0c49ccd196a30712d2726f
Mason <seifzellaban@gmail.com>
2025-02-15 23:13:12 +0200

    Setup api routes

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
-import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 
 const app = express();
 
@@ -9,9 +12,10 @@ dotenv.config();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
-
+app.use("/api/message", messageRoutes);
 app.listen(PORT, () => {
   console.log(`Server is running on PORT:${PORT}`);
   connectDB();
