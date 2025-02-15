56bab8874fb5aa8b1b62f57bc58325cfa7d1adea
Sherif <127263385+sizif-22@users.noreply.github.com>
2025-02-16 01:53:43 +0200

    creating a socket connection

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,45 @@
+"use client";
+import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
 export default function Home() {
+  const [message, setMessage] = useState("");
+  const socket = io("http://localhost:5001");
+  const handleSubmition = () => {
+    socket.send(message);
+  };
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("connected");
+    });
+    return () => socket.disconnect();
+  }, []);
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>Hello world!</h1>
+    <div className=" bg-slate-700 h-screen w-screen flex gap-5 items-center justify-center flex-col">
+      <h1 className=" font-bold text-white">Welcome to Convo!</h1>
+
+      <form
+        className="flex  gap-2 h-10"
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSubmition();
+        }}
+      >
+        <input
+          type="text"
+          placeholder="Your Message!"
+          value={message}
+          onChange={(e) => {
+            setMessage(e.target.value);
+          }}
+          className="input input-bordered w-full max-w-xs h-full px-2 rounded  outline-none border-2 border-[rgba(0,0,0,0.40)]"
+        />
+        <button
+          className=" bg-slate-900 text-white  px-3 rounded h-full border-2 border-[rgba(0,0,0,0.40)] "
+          onClick={handleSubmition}
+        >
+          send
+        </button>
+      </form>
     </div>
   );
 }
--- src/server/index.js
+++ src/server/index.js
@@ -2,17 +2,36 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
-
-const app = express();
-
+import http from "http";
+import { Server } from "socket.io";
+// import cors from "cors";
 dotenv.config();
-const PORT = process.env.PORT;
+const app = express();
+const server = http.createServer(app);
 
+const PORT = process.env.PORT || 3001;
+// app.use(cors({origin:"*"}));
+// app.use(cors());
 app.use(express.json());
-
 app.use("/api/auth", authRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on PORT:${PORT}`);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+io.on("connection", (socket) => {
+  console.log(socket.id, " .connected ");
+
+  socket.on("message", (message) => {
+    console.log("message: ", message);
+  });
+});
+io.on("disconnection", (socket) => {
+  console.log(socket.id, " .disconnected ");
+});
+
+server.listen(PORT, "localhost", () => {
+  console.log(`http://localhost/${PORT}`);
   connectDB();
 });
